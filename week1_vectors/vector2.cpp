8f31ee99f554530a4b600e1569e82cd0b2420d0d
KowshikRaj A <kaushikprasad2005@gmail.com>
2026-03-08 22:26:13 +0700

    Vector2d basic implementation

--- week1_vectors/vector2.cpp
+++ week1_vectors/vector2.cpp
@@ -1,5 +1,5 @@
 #include "vector2.h"
-
+#include <cmath>
 Vector2::Vector2(double x, double y) {
     this->x = x;
     this->y = y;
@@ -19,4 +19,8 @@ Vector2 Vector2::multiply(const Vector2& other) {
 
 double Vector2::dot(const Vector2& other) {
     return (x * other.x + y * other.y);
+}
+
+double Vector2::magnitude() {
+    return std::sqrt(x * x + y * y);
 }
--- week1_vectors/vector2d.cpp
+++ week1_vectors/vector2d.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 int main() {
-    std::cin >> a.x >> a.y;
-    std::cin >> b.x >> b.y;
+    Vector2 a(0, 0);
+    Vector2 b(0, 0);
 
+    cin >> a.x >> a.y;  //Get the input for the vector a 
+    cin >> b.x >> b.y;  //Get the input for the vector b
 
-    Vector2 c = a.add(a);
-    Vector2 d = b.subtract(a);
-    Vector2 e = a.multiply(b);
+    Vector2 c = a.add(a); //Add the vector a to itself 
+    Vector2 d = b.subtract(a); //Subtract vector from b to a
+    Vector2 e = a.multiply(b); //Multiply the vector a with vector b
+    double f = a.dot(b); //Calculate the dot product of vector a and vector b 
+    double g = a.magnitude(); //Calculate the magnitude of vector a
 
     std::cout << c.x << " " << c.y << std::endl;
-    std::cout << d.x << " " << d.y << std::endl;
     std::cout << e.x << " " << e.y << std::endl;
+    std::cout << f << endl;
+    std::cout << g << endl;
 
     return 0;
 }
