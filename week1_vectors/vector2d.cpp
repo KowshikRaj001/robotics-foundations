06c9c7fca53c94612f3f262d92d07ddf3892ef7e
KowshikRaj A <kaushikprasad2005@gmail.com>
2026-03-08 22:39:31 +0700

    Vector2d basic implementation

--- week1_vectors/vector2d.cpp
+++ week1_vectors/vector2d.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include "vector2.h"
 using namespace std;
 
@@ -9,16 +10,18 @@ int main() {
     cin >> a.x >> a.y;  //Get the input for the vector a 
     cin >> b.x >> b.y;  //Get the input for the vector b
 
-    Vector2 c = a.add(a); //Add the vector a to itself 
+    Vector2 c = a.add(a);      //Add the vector a to itself 
     Vector2 d = b.subtract(a); //Subtract vector from b to a
     Vector2 e = a.multiply(b); //Multiply the vector a with vector b
-    double f = a.dot(b); //Calculate the dot product of vector a and vector b 
-    double g = a.magnitude(); //Calculate the magnitude of vector a
+    double f = a.dot(b);       //Calculate the dot product of vector a and vector b 
+    double g = a.magnitude();  //Calculate the magnitude of vector a
 
     std::cout << c.x << " " << c.y << std::endl;
     std::cout << e.x << " " << e.y << std::endl;
     std::cout << f << endl;
     std::cout << g << endl;
 
+    assert(a.magnitude() == 5); // Check if the magnitude of vector v is 5
+    
     return 0;
 }
