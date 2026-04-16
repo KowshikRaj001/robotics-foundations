a41e957e1ed03090bf5be4ccef6d3e577b8736fa
KowshikRaj A <kaushikprasad2005@gmail.com>
2026-04-16 10:37:06 -0400

    Implemented Vector3D class with dot product, cross product, normalization, and projection operations

---
+++ build/CMakeFiles/robotic_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robotic_cpp.dir/main.cpp.o"
+  "CMakeFiles/robotic_cpp.dir/main.cpp.o.d"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Matrix2D.cpp.o"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Matrix2D.cpp.o.d"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Transform2D.cpp.o"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Transform2D.cpp.o.d"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Vector2D.cpp.o"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Vector2D.cpp.o.d"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Vector3D.cpp.o"
+  "CMakeFiles/robotic_cpp.dir/robotic_cpp/src/Vector3D.cpp.o.d"
+  "robotic_cpp"
+  "robotic_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robotic_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
